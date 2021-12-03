9a1f34850f061a4ddbdf2c347288e36735d6f749
dpreid <dprydereid@gmail.com>
2021-12-03 12:49:43 +0000

    updated font order

--- src/App.vue
+++ src/App.vue
@@ -399,7 +399,7 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Arial, Avenir, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
