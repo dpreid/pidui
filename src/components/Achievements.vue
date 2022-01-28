9375174afe48156ceea0d957b426f2a10b867693
dpreid <dprydereid@gmail.com>
2022-01-28 14:05:20 +0000

    added in varying colour for achievements badge depending on number achieved

--- src/components/Achievements.vue
+++ src/components/Achievements.vue
@@ -1,15 +1,50 @@
 <template>
     <li class="nav-item dropdown">
         <button type='button' class='btn btn-primary dropdown-toggle' id='achievements-button' data-bs-toggle="dropdown" aria-expanded="false" @click='setAchievementUpdate(false)'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-award" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" :fill="getFillColour" class="bi bi-award" viewBox="0 0 16 16">
                 <path d="M9.669.864 8 0 6.331.864l-1.858.282-.842 1.68-1.337 1.32L2.6 6l-.306 1.854 1.337 1.32.842 1.68 1.858.282L8 12l1.669-.864 1.858-.282.842-1.68 1.337-1.32L13.4 6l.306-1.854-1.337-1.32-.842-1.68L9.669.864zm1.196 1.193.684 1.365 1.086 1.072L12.387 6l.248 1.506-1.086 1.072-.684 1.365-1.51.229L8 10.874l-1.355-.702-1.51-.229-.684-1.365-1.086-1.072L3.614 6l-.25-1.506 1.087-1.072.684-1.365 1.51-.229L8 1.126l1.356.702 1.509.229z"/>
                 <path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1 4 11.794z"/>
             </svg>
-            <span v-if='getAchievementUpdated' class="badge rounded-pill bg-danger" id='achievement-notification' role="status" aria-hidden="true">{{ getNewAchievementCount }}<span class="visually-hidden">unread messages</span></span> 
+            <span v-if='getAchievementUpdated' class='badge rounded-pill bg-danger' id='achievement-notification' role="status" aria-hidden="true">{{ getNewAchievementCount }}<span class="visually-hidden">unread messages</span></span> 
         </button>
         
         <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown2">
             <li><h4 class='text-muted text-center'>Achievements</h4></li>
+
+            <div class='d-flex row m-3 justify-items-center'>
+                <div class='col-1'>
+                    <p>50%+</p>
+                </div>
+                <div class='col-3'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="chocolate" class="bi bi-award" viewBox="0 0 16 16">
+                        <path d="M9.669.864 8 0 6.331.864l-1.858.282-.842 1.68-1.337 1.32L2.6 6l-.306 1.854 1.337 1.32.842 1.68 1.858.282L8 12l1.669-.864 1.858-.282.842-1.68 1.337-1.32L13.4 6l.306-1.854-1.337-1.32-.842-1.68L9.669.864zm1.196 1.193.684 1.365 1.086 1.072L12.387 6l.248 1.506-1.086 1.072-.684 1.365-1.51.229L8 10.874l-1.355-.702-1.51-.229-.684-1.365-1.086-1.072L3.614 6l-.25-1.506 1.087-1.072.684-1.365 1.51-.229L8 1.126l1.356.702 1.509.229z"/>
+                        <path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1 4 11.794z"/>
+                    </svg>
+                </div>
+                
+
+                <div class='col-1'>
+                    <p>75%+</p>
+                </div>
+                <div class='col-3'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="silver" class="bi bi-award" viewBox="0 0 16 16">
+                        <path d="M9.669.864 8 0 6.331.864l-1.858.282-.842 1.68-1.337 1.32L2.6 6l-.306 1.854 1.337 1.32.842 1.68 1.858.282L8 12l1.669-.864 1.858-.282.842-1.68 1.337-1.32L13.4 6l.306-1.854-1.337-1.32-.842-1.68L9.669.864zm1.196 1.193.684 1.365 1.086 1.072L12.387 6l.248 1.506-1.086 1.072-.684 1.365-1.51.229L8 10.874l-1.355-.702-1.51-.229-.684-1.365-1.086-1.072L3.614 6l-.25-1.506 1.087-1.072.684-1.365 1.51-.229L8 1.126l1.356.702 1.509.229z"/>
+                        <path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1 4 11.794z"/>
+                    </svg>
+                </div>
+
+                <div class='col-1'>
+                    <p>90%+</p>
+                </div>
+                <div class='col-3'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="gold" class="bi bi-award" viewBox="0 0 16 16">
+                        <path d="M9.669.864 8 0 6.331.864l-1.858.282-.842 1.68-1.337 1.32L2.6 6l-.306 1.854 1.337 1.32.842 1.68 1.858.282L8 12l1.669-.864 1.858-.282.842-1.68 1.337-1.32L13.4 6l.306-1.854-1.337-1.32-.842-1.68L9.669.864zm1.196 1.193.684 1.365 1.086 1.072L12.387 6l.248 1.506-1.086 1.072-.684 1.365-1.51.229L8 10.874l-1.355-.702-1.51-.229-.684-1.365-1.086-1.072L3.614 6l-.25-1.506 1.087-1.072.684-1.365 1.51-.229L8 1.126l1.356.702 1.509.229z"/>
+                        <path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1 4 11.794z"/>
+                    </svg>
+                </div>
+            </div>
+
+
             <li v-for='item in getAchievements' :key='item.name' class='dropdown-item'>
                 <div v-if='item.completed' class='form-check'>
                     <label :for="item.name" class="form-label">{{item.verbose}}</label>
@@ -50,6 +85,26 @@ export default {
             'getAchievementUpdated',
             'getNewAchievementCount',
         ]),
+        getFillColour(){
+            let count = 0;
+            let total = this.getAchievements.length;
+            this.getAchievements.forEach(achievement => {
+                if(achievement.completed){
+                    count += 1;
+                }
+            });
+            let frac = count / total;
+            if(frac >= 0.9){
+                return 'gold';
+            } else if(frac >= 0.75){
+                return 'silver';
+            } else if(frac >= 0.5){
+                return 'chocolate';
+            } else {
+                return 'white';
+            }
+            
+        }
     },
     methods:{
         ...mapActions([
